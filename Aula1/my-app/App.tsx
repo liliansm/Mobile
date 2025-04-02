d303e58f4c1a7da40f9bf2ee78accb091b3be29f
Lilian Souza de Melo <lilianmelo22022005@gmail.com>
2025-04-01 23:39:50 -0300

    Base do projeto, sem a estilização

--- Aula1/my-app/App.tsx
+++ Aula1/my-app/App.tsx
@@ -1,14 +1,15 @@
 import React from "react";
-import { View, TextInput, StyleSheet } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import { Avatar} from "react-native-elements";
 import { Input } from "react-native-elements";
 import { Button } from "react-native-elements";
+
 
 function Login() {
   return (
     <View style={styles.container}>
       <Avatar
-        size="xlarge"
+        size="large"
         rounded
         icon={{ name: "user", type: "font-awesome" }}
         source={{ uri: "https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg" }}
@@ -36,6 +37,64 @@ function Login() {
       containerStyle={styles.buttonContainer}
       />
 
+      <Text>Esqueceu a Senha</Text>
+
+    </View>
+  );
+}
+
+function Cadastro() {
+  return (
+    <View style={styles.container}>
+      
+      <Text>Cadastro</Text>
+
+      <Input
+        label="nome"
+        containerStyle={styles.inputContainer}
+        inputStyle={styles.input}
+      />
+      <Input
+        label="Email"
+        containerStyle={styles.inputContainer}
+        inputStyle={styles.input}
+      />
+        <Input
+        label="Senha"
+        containerStyle={styles.inputContainer}
+        inputStyle={styles.input}
+      />
+
+      <Button
+      title="Cadastrar"
+      buttonStyle={styles.button}
+      containerStyle={styles.buttonContainer}
+      />
+
+
+    </View>
+  );
+}
+
+function EsqueceuSenha() {
+  return (
+    <View style={styles.container}>
+      
+      <Text>Esqueceu a Senha</Text>
+      
+      <Input
+        label="Email"
+        containerStyle={styles.inputContainer}
+        inputStyle={styles.input}
+      />
+
+      <Button
+      title="Enviar"
+      buttonStyle={styles.button}
+      containerStyle={styles.buttonContainer}
+      />
+
+
     </View>
   );
 }
@@ -57,6 +116,7 @@ const styles = StyleSheet.create({
   button: {
     backgroundColor: "#149914",
     paddingHorizontal: 30,
+    borderRadius: 20,
   },
   buttonContainer: {
     width: "80%",
